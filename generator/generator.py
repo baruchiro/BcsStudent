cf42b6cdc74680fff90e71036eab2714ab21c2eb
Baruch Rothkoff <baruchiro@gmail.com>
2019-01-11 15:44:23 +0200

    Generate the v1 script

---
+++ generator/generator.py
@@ -0,0 +1,83 @@
+# -*- coding: utf-8 -*-
+
+import frontmatter
+import glob
+from typing import List
+import os
+from os import path
+from bs4 import BeautifulSoup, Tag
+from tinydb import TinyDB, Query
+
+
+os.chdir(path.dirname(path.dirname(__file__)))
+TERMS_LIST_TEMPLATE = open('generator/terms_list.html', 'r').read()
+NEW_TERM_MARK = '###'
+DEFAULT_DESCRIPTION = "עדיין אין פירוט למושג הזה"
+
+db = TinyDB('generator/db.json').table('Terms')
+Terms = Query()
+
+print(TERMS_LIST_TEMPLATE)
+
+
+def get_post(path: str)->frontmatter.Post:
+    return frontmatter.load(path)
+
+
+def get_md_in_folder(folder: str)->List[str]:
+    return glob.glob(f"{folder}/*.md")
+
+
+def get_new_terms_as_li(content)->List[Tag]:
+    def create_li(soup: BeautifulSoup, a)->Tag:
+        term_name = a["href"].replace(NEW_TERM_MARK, '')
+        terms = db.search(Terms.name == term_name)
+        if len(terms) == 0:
+            description = DEFAULT_DESCRIPTION
+            title = term_name
+        else:
+            description = terms[0]['description']
+            title = terms[0]['title']
+            if len(terms) > 1:
+                print(f'{term_name} appear in {len(terms)} times')
+
+        li = soup.new_tag('li')
+        li['term'] = term_name
+        li.append(f'<strong>{title}</strong>{description}')
+
+        return li
+
+    soup = BeautifulSoup(content, features="html.parser")
+    all_a = soup.select(f'a[href^={NEW_TERM_MARK}]')
+    return [create_li(soup, a) for a in all_a]
+
+
+def insert_li_to_list(content, li_list: List[Tag]):
+    if 'terms_div' not in content:
+        content = content + TERMS_LIST_TEMPLATE
+
+    soup = BeautifulSoup(content, features="html.parser")
+    ul = soup.find_all('ul', class_='terms_list')
+    if len(ul) == 0:
+        raise "Error adding list"
+    if len(ul) > 1:
+        print("There are more then 1 ul")
+
+    ul = ul[0]
+
+    for l in li_list:
+        ul.append(l)
+
+    return soup
+
+
+def rewrite_post(path: str, post: frontmatter.Post):
+    frontmatter.dump(post, path)
+
+
+posts = {p: get_post(p) for p in get_md_in_folder("_posts")}
+
+for path, post in posts.items():
+    new_terms_li = get_new_terms_as_li(post.content)
+    post.content = insert_li_to_list(post.content, new_terms_li)
+    rewrite_post(path, post)
