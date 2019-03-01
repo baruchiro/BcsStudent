c384eb4707775f488edaf77b11e2a219fc04ac18
Baruch Rothkoff <Baruch.Rothkoff@checkmarx.com>
2019-03-01 14:23:29 +0200

    Move to Blog class

--- generator/generator.py
+++ generator/generator.py
@@ -19,6 +19,50 @@ db = TinyDB('generator/db.json', encoding='utf-8').table('Terms')
 Terms = Query()
 
 
+class BlogPost(frontmatter.Post):
+    def __init__(self, content, path, handler=None, **metadata):
+        self = super().__init__(content, handler=handler, **metadata)
+        self.content = BeautifulSoup(self.content, features="html.parser")
+        self.path = path
+        return self
+
+    def get_new_terms_as_li(self) -> List[Tag]:
+
+        all_a = self.content.select(f'a[href^={NEW_TERM_MARK}]')
+        return [self.create_li(self.content, a, self['ID']) for a in all_a]
+
+    def __create_li(self, a, id) -> Tag:
+        term_name = a["href"].replace(NEW_TERM_MARK, '')
+        term = get_term_by_name(term_name)
+
+        li = self.content.new_tag('li')
+        li['term'] = term["name"]
+        li['id'] = f'{id}_{term["name"]}'
+        li.append(BeautifulSoup(
+            f'<strong>{term["title"]}</strong>- {term["description"]}.', features="html.parser"))
+
+        return li
+
+    def insert_li_to_list(self, li_list: List[Tag]):
+        if 'terms_div' not in self.content:
+            self.content = BeautifulSoup(
+                str(self.content) + str(TERMS_LIST_TEMPLATE), features="html.parser")
+
+        ul = self.content.find_all('ul', class_='terms_list')
+        if len(ul) == 0:
+            raise "Error adding list"
+        if len(ul) > 1:
+            print("There are more then 1 ul")
+
+        ul = ul[0]
+
+        for l in li_list:
+            ul.append(l)
+    
+    def rewrite_post(self):
+        frontmatter.dump(self, self.path)
+
+
 def get_term_by_name(name: str):
     name = name.lower()
     terms = db.search(Terms.name == name)
@@ -34,50 +78,14 @@ def get_term_by_name(name: str):
     return terms[0]
 
 
-def get_post(path: str)->frontmatter.Post:
-    post = frontmatter.load(path)
+def get_post(path: str)->BlogPost:
+    post = BlogPost(path)
     post.content = BeautifulSoup(post.content, features="html.parser")
     return post
 
 
 def get_md_in_folder(folder: str)->List[str]:
     return glob.glob(f"{folder}/*.md")
-
-
-def get_new_terms_as_li(post: frontmatter.Post)->List[Tag]:
-
-    def create_li(soup: BeautifulSoup, a, id)->Tag:
-        term_name = a["href"].replace(NEW_TERM_MARK, '')
-        term = get_term_by_name(term_name)
-
-        li = soup.new_tag('li')
-        li['term'] = term["name"]
-        li['id'] = f'{id}_{term["name"]}'
-        li.append(BeautifulSoup(
-            f'<strong>{term["title"]}</strong>- {term["description"]}.', features="html.parser"))
-
-        return li
-
-    all_a = post.content.select(f'a[href^={NEW_TERM_MARK}]')
-    return [create_li(post.content, a, post['ID']) for a in all_a]
-
-
-def insert_li_to_list(content: BeautifulSoup, li_list: List[Tag])->BeautifulSoup:
-    if 'terms_div' not in content:
-        content = BeautifulSoup(str(content) + str(TERMS_LIST_TEMPLATE), features="html.parser")
-
-    ul = content.find_all('ul', class_='terms_list')
-    if len(ul) == 0:
-        raise "Error adding list"
-    if len(ul) > 1:
-        print("There are more then 1 ul")
-
-    ul = ul[0]
-
-    for l in li_list:
-        ul.append(l)
-
-    return content
 
 
 def replace_all_terms_links(post: frontmatter.Post):
@@ -96,10 +104,6 @@ def update_terms_list(post: frontmatter.Post):
             f'<strong>{term["title"]}</strong>- {term["description"]}.', features="html.parser")
 
     return post.content
-
-
-def rewrite_post(path: str, post: frontmatter.Post):
-    frontmatter.dump(post, path)
 
 
 def regenerate_terms_page():
