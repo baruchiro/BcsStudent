a3d7c6570ec29f2a98cd74406022703cce0b8a81
Baruch Rothkoff <baruchiro@gmail.com>
2020-03-18 23:47:23 +0200

    Fix template

--- gridsome.config.js
+++ gridsome.config.js
@@ -5,8 +5,8 @@
 // To restart press CTRL + C in terminal and run `gridsome develop`
 
 module.exports = {
-  siteName: 'Gridsome Blog Starter',
-  siteDescription: 'A simple, hackable & minimalistic starter for Gridsome that uses Markdown for content.',
+  siteName: 'B.Cs Student',
+  siteDescription: 'Be a Computer Science student',
 
   templates: {
     Post: '/:date',
--- src/components/Author.vue
+++ src/components/Author.vue
@@ -8,12 +8,12 @@
 		</h1>
 
 		<p class="author__intro">
-			A simple, hackable & minimalistic starter for Gridsome that uses Markdown for content.
+			אמנם כבר לא סטודנט, אבל אוהב ללמוד דברים חדשים
 		</p>
 
 		<p class="author__links">
-			<a href="//twitter.com/gridsome">Follow on Twitter</a>
-			<a href="//github.com/gridsome/gridsome-starter-blog">GitHub</a>
+			<a href="mailto:baruchiro@gmail.com">Email</a>
+			<a href="//github.com/baruchiro/BcsStudent">GitHub</a>
 		</p>
 
 	</div>
