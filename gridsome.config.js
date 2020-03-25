c4dee143eb8abf7107926b1532ff8fc082aabca1
Baruch Rothkoff <baruchiro@gmail.com>
2020-03-25 18:47:05 +0200

    Add my name

--- gridsome.config.js
+++ gridsome.config.js
@@ -5,7 +5,7 @@
 // To restart press CTRL + C in terminal and run `gridsome develop`
 
 module.exports = {
-  siteName: 'B.Cs Student',
+  siteName: 'ברוך אודם - B.Cs Student',
   siteDescription: 'Be a Computer Science student',
 
   templates: {
