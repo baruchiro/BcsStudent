612d39bd1eb27d845919462fb5e429ed724403a6
Baruch Rothkoff <baruchiro@gmail.com>
2020-07-22 19:24:05 +0300

    Try add meta

---
+++ src/meta.js
@@ -0,0 +1,26 @@
+const prefixes = ['', 'og:', 'twitter:']
+
+const createMetaTags = (tagName, value) => prefixes.map((prefix) => ({
+    property: prefix + tagName,
+    // name: prefix + tagName,
+    content: value
+}))
+
+export default (title, description, image, imageWidth, imageHeight) => {
+
+    const meta = []
+
+    if (title) meta.push(...createMetaTags('title', title))
+    if (description) meta.push(...createMetaTags('description', description))
+    if (image) meta.push(...createMetaTags('image', image))
+
+    if (imageWidth) meta.push(...createMetaTags('image:width', imageWidth))
+    if (imageHeight) meta.push(...createMetaTags('image:height', imageHeight))
+
+    meta.push({
+        property: 'twitter:card',
+        content: image? 'summary_large_image' : 'summary'
+    })
+
+    return meta
+}
