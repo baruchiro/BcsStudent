2b84b6d0cb5fcb80b41cd3656510e8fb8859ea19
Baruch Odem <baruchiro@gmail.com>
2025-01-23 20:52:16 +0200

    fix title logo

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,6 +1,7 @@
+import profile from '@/data/baruch.png'
 import headerNavLinks from '@/data/headerNavLinks'
-import Logo from '@/data/logo.svg'
 import siteMetadata from '@/data/siteMetadata'
+import Image from './Image'
 import Link from './Link'
 import MobileNav from './MobileNav'
 import SearchButton from './SearchButton'
@@ -13,7 +14,13 @@ const Header = () => {
         <Link href="/" aria-label={siteMetadata.headerTitle}>
           <div className="flex items-center justify-between">
             <div className="ms-3">
-              <Logo />
+              <Image
+                src={profile.src}
+                alt="Profile"
+                width={48}
+                height={48}
+                className="rounded-full"
+              />
             </div>
             {typeof siteMetadata.headerTitle === 'string' ? (
               <div className="hidden h-6 text-2xl font-semibold sm:block">
--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -2,8 +2,8 @@
 const siteMetadata = {
   title: 'ברוך אודם',
   author: 'ברוך אודם',
-  headerTitle: 'TailwindBlog',
-  description: 'בלוג שנבנה עם Next.js ו-Tailwind.css',
+  headerTitle: 'ברוך אודם',
+  description: 'אמנם כבר לא סטודנט, אבל אוהב ללמוד דברים חדשים',
   language: 'he-IL',
   theme: 'system', // system, dark or light
   siteUrl: 'https://tailwind-nextjs-starter-blog.vercel.app',
