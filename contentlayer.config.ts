b27eab0d2d5cb5418cc7a118b3254b38e96ba645
Baruch Odem <baruchiro@gmail.com>
2025-02-08 19:35:38 +0200

    add heading link

--- contentlayer.config.ts
+++ contentlayer.config.ts
@@ -13,6 +13,7 @@ import {
 import remarkGfm from 'remark-gfm'
 import remarkMath from 'remark-math'
 // Rehype packages
+import { fromHtmlIsomorphic } from 'hast-util-from-html-isomorphic'
 import { sortPosts } from 'pliny/utils/contentlayer.js'
 import rehypeAutolinkHeadings from 'rehype-autolink-headings'
 import rehypeCitation from 'rehype-citation'
@@ -24,6 +25,18 @@ import siteMetadata from './data/siteMetadata'
 
 const root = process.cwd()
 const isProduction = process.env.NODE_ENV === 'production'
+
+const contentHeaderLinkIcon = fromHtmlIsomorphic(
+  `
+<span class="content-header-link">
+<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5 linkicon">
+<path d="M12.232 4.232a2.5 2.5 0 0 1 3.536 3.536l-1.225 1.224a.75.75 0 0 0 1.061 1.06l1.224-1.224a4 4 0 0 0-5.656-5.656l-3 3a4 4 0 0 0 .225 5.865.75.75 0 0 0 .977-1.138 2.5 2.5 0 0 1-.142-3.667l3-3Z" />
+<path d="M11.603 7.963a.75.75 0 0 0-.977 1.138 2.5 2.5 0 0 1 .142 3.667l-3 3a2.5 2.5 0 0 1-3.536-3.536l1.225-1.224a.75.75 0 0 0-1.061-1.06l-1.224 1.224a4 4 0 1 0 5.656 5.656l3-3a4 4 0 0 0-.225-5.865Z" />
+</svg>
+</span>
+`,
+  { fragment: true }
+)
 
 const computedFields: ComputedFields = {
   readingTime: { type: 'json', resolve: (doc) => readingTime(doc.body.raw) },
@@ -146,7 +159,16 @@ export default makeSource({
     ],
     rehypePlugins: [
       rehypeSlug,
-      rehypeAutolinkHeadings,
+      [
+        rehypeAutolinkHeadings,
+        {
+          behavior: 'prepend',
+          headingProperties: {
+            className: ['content-header'],
+          },
+          content: contentHeaderLinkIcon,
+        },
+      ],
       rehypeKatex,
       [rehypeCitation, { path: path.join(root, 'data') }],
       [rehypePrismPlus, { defaultLanguage: 'js', ignoreMissing: true }],
