7c3f20cfea41f78eae91a186871cab651f1aa909
Baruch Odem <baruchiro@gmail.com>
2025-02-14 08:16:36 +0200

    Revert "try pathname effect"
    This reverts commit 9c415e5662e37b32148dc28b9c305aa1843bd0ca.

--- src/components/AnalyticsProvider.tsx
+++ src/components/AnalyticsProvider.tsx
@@ -1,11 +1,14 @@
 'use client'
 
-import { usePageView } from '@/hooks/usePageView'
 import analytics from '@/lib/analytics'
+import { useEffect } from 'react'
 import { AnalyticsProvider as Provider } from 'use-analytics'
 
 export default function AnalyticsProvider({ children }: { children: React.ReactNode }) {
-  usePageView()
+  useEffect(() => {
+    // Track initial page view
+    analytics.page()
+  }, [])
 
   return <Provider instance={analytics}>{children}</Provider>
 }
